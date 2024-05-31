dbf29d8fd1e6a234970cc65cd43a69966823b911
Noyon Hossain <mdshahinurrahmannayon@gmal.com>
2024-05-31 21:34:08 +0600

    Blood Request Profile Data Done

--- src/app/(withDashboardLayout)/dashboard/user/blood-request/[donorRequest]/page.tsx
+++ src/app/(withDashboardLayout)/dashboard/user/blood-request/[donorRequest]/page.tsx
@@ -16,74 +16,72 @@ type TParams = {
     }
 }
 
-// export const validationSchema = z.object({
-//     phoneNumber: z.string().min(11, "Must be 11 characters"),
-//     hospitalName: z.string({ required_error: "Please enter your hospital name!" }),
-//     hospitalAddress: z.string({ required_error: "Please enter a hospital address!" }),
-//     reason: z.string({ required_error: "Please enter your reason!" }),
-//     dateOfDonation: z.string({ required_error: "Please enter the date of donation!" }),
-// });
+export const validationSchema = z.object({
+    phoneNumber: z.string().min(11, "Must be 11 characters"),
+    hospitalName: z.string({ required_error: "Please enter your hospital name!" }),
+    hospitalAddress: z.string({ required_error: "Please enter a hospital address!" }),
+    reason: z.string({ required_error: "Please enter your reason!" }),
+    dateOfDonation: z.string({ required_error: "Please enter the date of donation!" }),
+});
 
-// export const defaultValueInfo: Record<string, any> = {
-//     phoneNumber: "",
-//     hospitalName: "",
-//     hospitalAddress: "",
-//     reason: "",
-//     dateOfDonation: ""
-// }
+export const defaultValueInfo: Record<string, any> = {
+    phoneNumber: "",
+    hospitalName: "",
+    hospitalAddress: "",
+    reason: "",
+    dateOfDonation: ""
+}
 
 const BloodRequest = ({ params }: TParams) => {
-    // const { data: myProfileData, isLoading } = useGetMYProfileQuery({});
-    // const [createDonorRequest] = useCreateDonorRequestMutation();
-    // const router = useRouter();
+    const { data: myProfileData, isLoading } = useGetMYProfileQuery({});
+    const [createDonorRequest] = useCreateDonorRequestMutation();
+    const router = useRouter();
 
-    // if (isLoading) {
-    //     return <Typography variant="h1" textAlign="center">Loading...</Typography>
-    // }
+    if (isLoading) {
+        return <Typography variant="h1" textAlign="center">Loading...</Typography>
+    }
 
-    // const handleBloodRequest = async (values: FieldValues) => {
-    //     values.donorId = params.donorRequest;
-    //     const res = await createDonorRequest(values);
-    //     if ("data" in res && res?.data?.id) {
-    //         toast.success("Blood Donor Request Successfully!");
-    //         router.push('/dashboard/user/blood-request-list');
-    //     }
-    // }
+    const handleBloodRequest = async (values: FieldValues) => {
+        values.donorId = params.donorRequest;
+        const res = await createDonorRequest(values);
+        if ("data" in res && res?.data?.id) {
+            toast.success("Blood Donor Request Successfully!");
+            router.push('/dashboard/user/blood-request-list');
+        }
+    }
 
     return (
-        <div>
-            {/* <Stack >
-                <Box sx={{ maxWidth: 600, width: "100%", boxShadow: 1, borderRadius: 1, padding: 4, textAlign: "center" }}>
-                    <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
-                        <Box>
-                            <Typography variant="h6" fontWeight={600}>Request With Blood Donor</Typography>
-                        </Box>
-                    </Stack>
+        <Stack >
+            <Box sx={{ maxWidth: 600, width: "100%", boxShadow: 1, borderRadius: 1, padding: 4, textAlign: "center" }}>
+                <Stack sx={{ justifyContent: "center", alignItems: "center" }}>
                     <Box>
-                        <PHForm onSubmit={handleBloodRequest} resolver={zodResolver(validationSchema)} defaultValues={defaultValueInfo}>
-                            <Grid container spacing={2} my={1}>
-                                <Grid item md={6}>
-                                    <PHInput type="number" name="phoneNumber" label="Phone Number" fullWidth={true} />
-                                </Grid>
-                                <Grid item md={6}>
-                                    <PHInput name="hospitalName" label="Hospital Name" fullWidth={true} />
-                                </Grid>
-                                <Grid item md={6}>
-                                    <PHInput name="hospitalAddress" label="Hospital Address" fullWidth={true} />
-                                </Grid>
-                                <Grid item md={6}>
-                                    <PHInput name="reason" label="Reason" fullWidth={true} />
-                                </Grid>
-                                <Grid item md={6}>
-                                    <PHInput type="date" name="dateOfDonation" label="Date Of Donation" fullWidth={true} />
-                                </Grid>
-                            </Grid>
-                            <Button type="submit" fullWidth sx={{ margin: "20px 0 15px 0" }}>Send Blood Request</Button>
-                        </PHForm>
+                        <Typography variant="h6" fontWeight={600}>Request With Blood Donor</Typography>
                     </Box>
+                </Stack>
+                <Box>
+                    <PHForm onSubmit={handleBloodRequest} resolver={zodResolver(validationSchema)} defaultValues={defaultValueInfo}>
+                        <Grid container spacing={2} my={1}>
+                            <Grid item md={6}>
+                                <PHInput type="number" name="phoneNumber" label="Phone Number" fullWidth={true} />
+                            </Grid>
+                            <Grid item md={6}>
+                                <PHInput name="hospitalName" label="Hospital Name" fullWidth={true} />
+                            </Grid>
+                            <Grid item md={6}>
+                                <PHInput name="hospitalAddress" label="Hospital Address" fullWidth={true} />
+                            </Grid>
+                            <Grid item md={6}>
+                                <PHInput name="reason" label="Reason" fullWidth={true} />
+                            </Grid>
+                            <Grid item md={6}>
+                                <PHInput type="date" name="dateOfDonation" label="Date Of Donation" fullWidth={true} />
+                            </Grid>
+                        </Grid>
+                        <Button type="submit" fullWidth sx={{ margin: "20px 0 15px 0" }}>Send Blood Request</Button>
+                    </PHForm>
                 </Box>
-            </Stack> */}
-        </div>
+            </Box>
+        </Stack>
     );
 };
 
